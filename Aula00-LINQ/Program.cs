1034cdd2f5daade88c8607c4c77286d10b45746a
Fabio <Fabio@DELL-FABIO>
2024-11-13 22:05:35 -0300

    Add project files.

---
+++ Aula00-LINQ/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Aula00_LINQ
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            List<int> numeros = new List<int> { 1, 2, 3, 4, 5 };
+
+            var pares = from n in numeros
+                        where n % 2 == 0
+                        select n;
+
+            foreach (var numero in pares)
+            {
+                Console.WriteLine(numero);
+            }
+
+            Console.WriteLine("Pressione Enter...");
+            Console.ReadKey();
+        }
+    }
+}
